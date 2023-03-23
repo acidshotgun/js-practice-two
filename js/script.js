eb4aae99e89d0f712032b9ccb845eb83c523f2d2
Sasha <example@gmail.com>
2023-03-23 18:24:35 +0300

    dots and functions

--- js/script.js
+++ js/script.js
@@ -430,6 +430,8 @@ window.addEventListener('DOMContentLoaded', () => {
     //      И при нажатии кнопок мы будет не скрывать/показывать а двигать это карусель влево/вправо
 
     const slides = document.querySelectorAll('.offer__slide'),
+          // Весь блок слайдера с точками и стрелками
+          slider = document.querySelector('.offer__slider'),
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
@@ -445,6 +447,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Эта переменная будет показывать на сколько мы отступили вправо/влево при помощи tranform
     let offset = 0;
 
+    // Условие для отображения чисел с 0 или без
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
         current.textContent = `0${slideIndex}`;
@@ -452,6 +455,8 @@ window.addEventListener('DOMContentLoaded', () => {
         total.textContent = slides.length;
         current.textContent = slideIndex;
     }
+
+
 
     // Кстанавливаем ширину карусели по отношению к кол-ву слайдов (кол-во слайдов умножить на 100%)
     // Те ширина карусели будет равна ширине всех слайдов стоящих подряд в линию
@@ -471,6 +476,73 @@ window.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    // Точки у слайдера
+    // Блоку позиционирование чтобы точки можно было располагатьт абсолютно
+    slider.style.position = 'relative';
+
+    // Создаем обертку для всех точек и стилизуем затем помещаем в сам блок слайдера
+    const indicators = document.createElement('ol'),
+          // Создадим массив для точек чтобы работать с ними в обработчиках событий при переключении слайдов
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0; 
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    // Создаем точки. Их кол-во будет зависеть от кол-ва самих слайдов
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        // Ставим каждой точке атрибут с нумерацией от 1 и стилизуем
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        // Помещаем точки в обертку для точек
+        indicators.append(dot);
+        // Добавляем точки в массив
+        dots.push(dot);
+    }
+
+    // Ф-я условия показа 0 при однозначных current числах
+    function checkCurrentNumbers(current) {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    // Ф-я движения блока карусели в сторону на offset пикселей т.е двигается влево на offset px
+    function setTranslateCarousel(offset) {
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    }
+
     // Скрпит сдвига по нажатию
     next.addEventListener('click', () => {
         // Условие при котором слайдер возвращается на первый слайд
@@ -482,7 +554,8 @@ window.addEventListener('DOMContentLoaded', () => {
             offset += +width.slice(0, width.length - 2);
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
+        // Движение блока карусели в сторону на offset пикселей т.е двигается влево на offset px
+        setTranslateCarousel(offset);
 
         // Условия при котором счетчик будет возвращаться в положение 1 если next на последнем слайде
         // И else он просто увеличивается
@@ -492,12 +565,9 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        // Условия показа 0 при однозначных числах
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        // Ф-я условия показа 0 при однозначных current числах
+        checkCurrentNumbers(current);
+        setDotOpacity(dots);
     });
 
     prev.addEventListener('click', () => {
@@ -509,7 +579,8 @@ window.addEventListener('DOMContentLoaded', () => {
             offset -= +width.slice(0, width.length - 2);
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
+        // Движение блока карусели в сторону на offset пикселей т.е двигается влево на offset px
+        setTranslateCarousel(offset);
 
         // Тут условие если мы находимся на 1 слайде и проматываем влево и попадаем на последний слайд
         if (slideIndex == 1) {
@@ -518,13 +589,35 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        // Условия показа 0 при однозначных числах
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        // Ф-я условия показа 0 при однозначных current числах
+        checkCurrentNumbers(current);
+        setDotOpacity(dots);
     });
+    
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            // Движение блока карусели в сторону на offset пикселей т.е двигается влево на offset px
+            setTranslateCarousel(offset);
+
+            // Ф-я условия показа 0 при однозначных current числах
+            checkCurrentNumbers(current);
+            //Ф-я перебора точек и установка прозрачности
+            setDotOpacity(dots);
+        });
+    });
+
+    // Ф-я перебора точек и установка прозрачности
+    // Перебираем точки и всем ставим прозрачность 50%
+    // А та точка которая по интексу совпадает со слайдом будет иметь прозрачность 1
+    function setDotOpacity(dots) {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
 });
 
 
