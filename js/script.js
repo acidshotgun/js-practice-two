b39d7ff34ce5ac836991b0635b1de2b52537b4bc
Sasha <example@gmail.com>
2023-03-25 18:39:04 +0300

    calc

--- js/script.js
+++ js/script.js
@@ -626,6 +626,121 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
     }
+
+
+    // Calc
+
+    // Результат колорий
+    const result = document.querySelector('.calculating__result span');
+    // Параметры которые будут меняться
+    // Ставим зн-я sex и  ratio по умолчанию чтобы изначально они как бы ыбли уже нажаты
+    // Иначе девушка с умеренной подвижностью введет только цифровые данные и ничего меняться не будет
+        // Там нужно будет еще раз кликать на пол и умеренную активность
+    let sex = 'female',
+        height, weight, age, 
+        ratio = 1.375;
+
+
+    // У нас будет две функции одна будет брать значения из инпутов вторая из блоков с активностью дневной
+    
+    // Ф-я подсчета конечного результат (основная)
+    // Для начала мы проверим условия чтобы все нужные параметры были заполнены
+    // Те если они не заполнены то == false. Используем (!)
+    // Эта ф-я будет вызываться после каждого обновления параметров клик и ввод
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            // Такой прием с return используется чтобы прервать функцию тогда код ниже идти не будет
+            return;
+        }
+
+        // Далее исходим из того какой пол выбран
+        // А формула взята из инета
+        // округляем до ближайшего целого Math.round
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    // Вызываем чтобы очистить поле с калориями
+    calcTotal();
+
+    // Теперь ф-ии получения данных
+    // Первая ф-я будет получать данные со статического контента, те где просто нужно выбрать суточную активность
+    // Аргументы это селектор и класс активности
+    function getStaticInformation(parentSelector, activeClass) {
+        // Получаем элементы внутри блока
+        // Получаем все div внутри этого родителя
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        // Перебором вешаем на каждый элемент слушатель
+        elements.forEach(elem => {
+            elem.addEventListener('click', (event) => {
+                // У выбора пола и активности разные атрибуты
+                // Активность имеет дата атрибут а пол id
+                // Тут прописываем условия при клике и получаем разные атрибуты
+                // При клике на активность будем получать значение активности
+                if (event.target.getAttribute('data-ratio')) {
+                    ratio = +event.target.getAttribute('data-ratio');
+                } else {
+                    // При клике на пол будем получать в sex пол
+                    sex = event.target.getAttribute('id');
+                }
+    
+                // Убираем все классы активности которые передаем в функцию
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                // Затем класс активности назначаем нажатому элементу
+                event.target.classList.add(activeClass);
+    
+                // Вызываем чтобы ф-я чтобы значение калорий менялось после каждого обновления параметров
+                calcTotal();
+            });
+        });
+    }
+
+    // Вызываем эту функцию для двух статичных блоков с аргументами (сам блок и класс активности при клике)
+    // 1) Это будет по id gender 2) Это класс активности для выделения (это все в верстке)
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+
+    // Пишем ф-я которая будет собирать данные с введенных параметров (которые надо вводить)
+    // Аргементы это селектр инпута
+    function getDynamicInformation(selector) {
+        // Получаем тот инпут с которым работаем
+        const input = document.querySelector(selector);
+
+        // Обработчик ввода для поля в котором происходит ввод
+        input.addEventListener('input', () => {
+            // Чтобы понять какой именно инпут заполняется в данный момент можно использовать switch case
+            // В условии мы получаем атрибут выбранного элемента по id и указываем д-я для каждлого условия (инпута)
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            // Теперь ф-я ореинтируется на инпут и ореинтируя на id записывает данные в опр переменную
+
+            // Вызываем чтобы ф-я чтобы значение калорий менялось после каждого обновления параметров
+            calcTotal();
+        });
+    }
+
+    // Теперь вызываем с трея разными селекторами чтобы работала
+    getDynamicInformation('#weight');
+    getDynamicInformation('#height');
+    getDynamicInformation('#age');
 });
 
 
